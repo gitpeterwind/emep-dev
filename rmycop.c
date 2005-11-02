932d678c9c1a14bdf431b26dbe6f7c8e68ef2069
David Simpson <davids@met.no>
2005-11-02 07:00:08 +0000

    rv2_4

--- rmycop.c
+++
@@ -1,6 +0,0 @@
-# include <string.h>
-void RMYCOPSTRITE(char *out,const char *in,int *n,int *outstr,int *instr)
-{
-	memcpy(out+*(outstr),in+(*instr),*n);
-	/*bcopy(in+(*instr),out+*(outstr),*n);*/
-}
