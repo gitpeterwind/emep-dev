307c7c51a7389f1baa16008a114cab2c4dec5df5
David Simpson <davids@met.no>
2010-11-05 13:56:08 +0000

    Soil water switched on

--- ModelConstants_ml.f90
+++ ModelConstants_ml.f90
@@ -41,7 +41,7 @@ module ModelConstants_ml
 ! Some flags for model setup
 ! will be removed when convection is sufficiently tested (use foundconv in permanent code)
  logical, public, parameter :: USE_CONVECTION   = .false.
- logical, public, parameter :: USE_SOILWATER    = .false.
+ logical, public, parameter :: USE_SOILWATER    = .true.
  logical, public, parameter :: USE_FOREST_FIRES = .false.
 ! Biogenics. Use 3 even if no terpene chemistry - simplifies
 ! rest of code.
