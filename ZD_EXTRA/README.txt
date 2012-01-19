92a0e99413c2f1998ef9ef2b5f44181bbcc5f12f
David Simpson <davids@met.no>
2012-01-19 09:25:56 +0000

    New Nest_ml - My_ExternalBICs_ml system

---
+++ ZD_EXTRA/README.txt
@@ -0,0 +1,23 @@
+ZD_EXTRA
+
+Extra files needed for specific project-chemistry combinations
+
+
+
+External Boundary Condition files
+
+
+So far:
+MOZART_EmChem09_
+
+
+Usage examples:
+
+
+ *After*  cp ZD_OZONE/My* 
+
+
+ cp ZD_EXTRA/MOZART_EmChem09_ExternalBICs_ml.f90  My_ExternalBICs_ml.f90
+
+ (ie we overwrite the default do-nothing My_ExternalBICs_ml with the specific
+  file we need)
---
+++ ZD_OZONE/My_ExternalBICs_ml.f90
@@ -0,0 +1,34 @@
+module My_ExternalBICs_ml
+!EXTERNAL Boundary and Initial Conditions:
+! Here = Dummy values - when BICs set using EMEP defaults
+! Replace this module by eg MOZART_ExternalBICs_ml if using
+! input from other (eg global) models.
+ use Io_ml, only : PrintLog
+ implicit none
+ private
+
+  public :: set_extbic
+
+  logical, public, parameter :: EXTERNAL_BIC_SET  = .false.
+  logical, public, parameter :: TOP_BC = .false.
+  character(len=30),public, parameter :: EXTERNAL_BIC_NAME = "DUMMY"
+
+  type, public :: icbc                 ! Inital (IC) & Boundary Conditions (BC)
+    character(len=24) :: varname=""
+    logical           :: wanted=.false.,found=.false.
+  end type icbc
+
+  type, public :: adv_icbc             ! IC/BC Set, included intended ixadv
+    integer           :: ixadv=-1
+    type(icbc)        :: icbc
+  end type adv_icbc
+
+
+  type(adv_icbc), dimension(9:0), public, save :: EXTERNAL_BC
+
+contains
+  subroutine set_extbic()
+     call PrintLog("No external BICs set")
+  end subroutine set_extbic
+
+end module My_ExternalBICs_ml
