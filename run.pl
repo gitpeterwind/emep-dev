e6c4522e77f6a4fec7205a6704314f3df5e358fd
David Simpson <davids@met.no>
2012-02-28 08:36:04 +0000

    rv3_10_22 changes

--- run.pl
+++ run.pl
@@ -255,7 +255,7 @@ my $VBS   = 0;
 my $Chem     = "EmChem09soa";
 #$Chem     = "CRI_v2_R5";
 
-my $testv = "rv3_10_21";
+my $testv = "rv3_10_22";
 
 #User directories
 my $ProgDir  = "$HOMEROOT/$USER/Unify/Unimod.$testv";   # input of source-code
