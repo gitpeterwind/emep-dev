b005123934029f3bba95f11403f7821a51e5418f
Hilde Fagerli <hildef@met.no>
2012-06-07 10:48:53 +0000

    new Ndep file

--- run.pl
+++ run.pl
@@ -868,7 +868,7 @@ print "TESTING PM $poll $dir\n";
 if ( $iyr_trend > 2015 )  {
   $ifile{"$DataDir/AnnualNdep_TNO28_2020.nc"} = "annualNdep.nc";
 } else {
-  $ifile{"$DataDir/annualNdep_tmp.nc"} = "annualNdep.nc";
+  $ifile{"$DataDir/AnnualNdep_PS50_EECCA.nc"} = "annualNdep.nc";
 }
 
 # hb NH3emis
