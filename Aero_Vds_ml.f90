3304fd1fea49b356e67300756cd70efa55ded50c
David Simpson <davids@met.no>
2012-09-11 18:23:21 +0000

    debug change

--- Aero_Vds_ml.f90
+++ Aero_Vds_ml.f90
@@ -3,7 +3,7 @@
 !==============================================================================
   use PhysicalConstants_ml, only : FREEPATH, VISCO, BOLTZMANN, PI, GRAV, ROWATER
   use My_Aerosols_ml,       only : NSIZE
-  use ModelConstants_ml,    only : DEBUG_VDS
+  use ModelConstants_ml,    only : DEBUG_VDS, MasterProc
  
   ! DESCRIPTION
   ! Calculates laminar sub-layer resistance (rb) and gravitational settling 
@@ -102,7 +102,11 @@ contains
        !... Settling velocity for poly-disperse 
         vs(imod) = vs_help*(exp(8.0*lnsig2)+1.246*knut*exp(3.5*lnsig2)) ! A31, k=3
 
-        if (DEBUG_VDS) write(6,'(a19,i3,f8.3)') "** Settling Vd **",imod,vs(imod)*100.0
+        if (DEBUG_VDS.and.MasterProc ) &
+            write(6,'(a,i3,es12.3,f10.3,5es12.3,3f9.2,f9.3)') &
+             "** Settling Vd ** ", imod, roa, tsK, &
+             dg,knut,Di_help,vs_help,Di, lnsig2, &
+             1.0e6*diam(imod), PMdens(imod), sigma(imod), vs(imod)*100.0
 
      end do !imod
    end function SettlingVelocity
