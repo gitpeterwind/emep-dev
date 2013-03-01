3b6f0b5cff16927ededc1d0cecb037f40d0eac15
Heiko Klein <heikok@met.no>
2013-03-01 10:48:28 +0000

    putting back positive="down" attribute after re-reading cf-convention

--- NetCDF_ml.f90
+++ NetCDF_ml.f90
@@ -376,6 +376,7 @@ endif
 !pwsvs for CF-1.0
   call check(nf90_put_att(ncFileID, kVarID, "standard_name", "atmosphere_sigma_coordinate"))
   call check(nf90_put_att(ncFileID, kVarID, "formula_terms", trim(vert_coord)))
+  call check(nf90_put_att(ncFileID, kVarID, "positive", "down"))
   call check(nf90_def_var(ncFileID, "PT", nf90_float,  varID = PTVarID) )
   call check(nf90_put_att(ncFileID, PTVarID, "units", "hPa"))
   call check(nf90_put_att(ncFileID, PTVarID, "long_name", "Pressure at top"))
