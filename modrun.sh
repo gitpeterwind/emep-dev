ad413a5a2992c7cba147a9d1ae128f0cf1212854
Peter Wind <peterw@met.no>
2015-10-08 09:58:29 +0000

    simplified modrun.sh

--- modrun.sh
+++ modrun.sh
@@ -7,25 +7,8 @@ inputdir=.
 ln -s $inputdir/met/*   .   # Driving meteorology
 ln -s $inputdir/input/* .   # Other input files
 
-# Define some run parameters
-trendyear=2008              # emission year
-runlabel1=Base              # short label
-runlabel2=Opensource_setup  # long label
-startdate="2008 01 01"      # start date (metdata)
-  enddate="2008 01 01"      # end date (metdata)
-
-# Put the run parameters in a temporary file
-cat > INPUT.PARA << EOF
-$trendyear
-$runlabel1
-$runlabel2
-$startdate
-$enddate
-EOF
-
 # Run the model
-mpirun $inputdir/code/Unimod
+mpiexec $inputdir/code/Unimod
 
-# Clean the links to the input data and remove INPUT.PARA
+# Clean the links to the input data
 find . -type l -print0 | xargs -0 rm 
-rm INPUT.PARA
