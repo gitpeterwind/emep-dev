96f2f051ae14f736da5b50c6f06d97de1f45ecac
Michael Gauss <michaelg@service1.ib0.vilje.hpc.ntnu.no>
2017-12-02 15:03:03 +0100

    switched from USED_XX to USES method

--- AOD_PM_ml.f90
+++ AOD_PM_ml.f90
@@ -20,7 +20,7 @@ use ChemGroups_ml,        only: chemgroups
 use CheckStop_ml,         only: CheckStop
 use GridValues_ml,        only: i_fdom, j_fdom
 use MetFields_ml,         only: z_bnd
-use ModelConstants_ml,    only: KMAX_MID, KCHEMTOP, ANALYSIS, USE_AOD
+use ModelConstants_ml,    only: KMAX_MID, KCHEMTOP, ANALYSIS, USES
 use Par_ml,               only: LIMAX,LJMAX   ! => x, y dimensions
 use PhysicalConstants_ml, only: AVOG
 use Setup_1dfields_ml,    only: xn_2d, rh
@@ -397,7 +397,7 @@ subroutine AOD_Ext(i,j,debug)
 
   if(first_call)then
     call AOD_init("AOD_Ext")
-    call CheckStop(USE_AOD.and..not.any(wanted_wlen(:)),&
+    call CheckStop(USES%AOD.and..not.any(wanted_wlen(:)),&
       "USE_AOR=T, but no AOD/EXT output. Check config_*.nml")
     first_call=.false.
   end if
