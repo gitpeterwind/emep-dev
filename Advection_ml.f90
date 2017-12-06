fd4f770b63859e37e4863c0217cf821cc51f7342
David Simpson <david.simpson@met.no>
2017-12-06 16:27:42 +0100

    Changing ModelConstants to Config_module

--- Advection_ml.f90
+++ Advection_ml.f90
@@ -77,7 +77,7 @@
                                 extendarea_N
   use Io_ml,             only : datewrite
   use Io_Progs_ml,       only : PrintLog
-  use ModelConstants_ml, only : KMAX_BND,KMAX_MID,NMET, nstep, nmax, &
+  use Config_module, only : KMAX_BND,KMAX_MID,NMET, nstep, nmax, &
                   dt_advec, dt_advec_inv,  PT,Pref, KCHEMTOP, NPROCX,NPROCY,NPROC, &
                   FORECAST,&
                   USES,DEBUG_ADV,USE_uEMEP,uEMEP,ZERO_ORDER_ADVEC
@@ -1183,7 +1183,7 @@
 !     executes advection with a. bott's integreated flux-form
 !     using 2'nd order polynomial in the vertical.
 
-    use ModelConstants_ml   , only : EPSIL, dt_advec
+    use Config_module   , only : EPSIL, dt_advec
     use ChemSpecs,         only : NSPEC_ADV
     implicit none
 
