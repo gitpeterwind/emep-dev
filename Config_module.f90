8a2749cd2f6e78522f4506f9d211928dd587ed3e
David Simpson <david.simpson@met.no>
2017-12-07 08:23:51 +0100

    Moved emep_Config_mod into Config_module

--- Config_module.f90
+++ Config_module.f90
@@ -8,7 +8,7 @@ module Config_module
 use Aerofunctions,        only: DpgV2DpgN
 use CheckStop_ml,         only: CheckStop
 use ChemSpecs,            only: species
-use emep_Config_mod,      only: PBL, EmBio, YieldModifications, LandCoverInputs, END_OF_EMEPDAY
+!!use emep_Config_mod,      only: PBL, EmBio, YieldModifications, LandCoverInputs, END_OF_EMEPDAY
 use Io_Nums_ml,           only: IO_NML, IO_LOG, IO_TMP
 use OwnDataTypes_ml,      only: typ_ss, uEMEP_type
 use Precision_ml,         only: dp
@@ -35,6 +35,44 @@ public :: Config_ModelConstants
 ! now be anything descriptive.
 CHARACTER(LEN=30), public, save :: EXP_NAME="EMEPSTD"
 CHARACTER(LEN=30), public, save :: MY_OUTPUTS="EMEPSTD"
+
+! EMEP daily measurements end at 6am, hence we typically adjust
+! for that. For global though, zero would be more normal
+  integer, save, public :: END_OF_EMEPDAY = 6 ! 
+
+  type, private :: PBL_t
+    real :: ZiMIN = 100.0                     ! minimum mixing height
+    real :: ZiMAX = 3000.0                    ! maximum mixing height
+    character(len=10) :: HmixMethod = "JcRb"  ! Method used for Hmix
+      ! JcRb = Jericevic/Richardson number method
+      ! "SbRb"= Seibert !"TIZi" = Original from Trond Iversen tiphysics
+  end type PBL_t
+  type(PBL_t), public, save :: PBL = PBL_t()
+
+  type, private :: EmBio_t
+    character(len=10) :: GlobBvocMethod = '-' ! can be MEGAN
+    real :: IsopFac = 1.0                     ! for experiments
+    real :: TerpFac = 1.0                     ! for experiments
+  ! canopy light factor, 1/1.7=0.59, based on Lamb 1993 (cf MEGAN 0.57)
+    real :: CLF     = 1.0                     ! canopy factor, leaf vs branch emissions
+  end type EmBio_t
+  type(EmBio_t), public, save :: EmBio = EmBio_t()
+
+ ! We allow a flexible string which can switch between different
+ ! experiments called by e.g. Solver. A but crude, but
+ ! it makes sure the experiments are recorded in the config
+ ! system
+
+  character(len=100), save, public :: YieldModifications = 'VBS' ! Default for EmChem16mt
+
+  
+  type, private :: LandCoverInputs_t
+    character(len=200), dimension(2) :: MapFile = 'NOTSET'  ! Usually PS European + global
+    character(len=200) :: LandDefs = '-'   !  LAI, h, etc (was Inputs_LandDefs
+    character(len=200) :: Do3seDefs = '-'  !  DO3SE inputs
+  end type LandCoverInputs_t
+  type(LandCoverInputs_t), public, save :: LandCoverInputs=LandCoverInputs_t()
+
 
 ! Namelist controlled:
 ! Some flags for model setup
