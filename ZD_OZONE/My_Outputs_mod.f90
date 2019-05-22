1367e5b7b68e5670d84042bbbfb6df0c7fdaa343
David Simpson <david.simpson@met.no>
2019-05-22 16:03:37 +0200

    modify sites outputs for Bosco met

--- ZD_OZONE/My_Outputs_mod.f90
+++ ZD_OZONE/My_Outputs_mod.f90
@@ -20,7 +20,7 @@ use Config_module, only: PPBINV, PPTINV, MasterProc, KMAX_MID,&
 use PhysicalConstants_mod, only: ATWAIR
 use OwnDataTypes_mod,   only: Asc2D
 use Par_mod,            only: GIMAX,GJMAX,IRUNBEG,JRUNBEG,me
-use SmallUtils_mod,     only: find_index
+use SmallUtils_mod,     only: find_index, find_duplicates
 use TimeDate_mod,       only: date
 use Units_mod,          only: Init_Units,&
                              to_molec_cm3,to_molec_cm2,to_mgSIA,to_ugSIA,&
@@ -42,15 +42,17 @@ integer, public, parameter :: &
    NSITES_MAX =        99     & ! Max. no surface sites allowed
   ,FREQ_SITE  =         1     & ! Interval (hrs) between outputs
   ,NADV_SITE  = NSPEC_ADV     & ! No. advected species (1 up to NSPEC_ADV)
-  ,NSHL_SITE  = NSPEC_SHL     & ! No. short-lived species
+  ,NSHL_SITE  =        1      & ! Bosco OH NSPEC_SHL     & ! No. short-lived species
   ,NXTRA_SITE_MISC =    2     & ! No. Misc. met. params  ( e.g. T2, d_2d)
-  ,NXTRA_SITE_D2D  =  9+8       ! No. Misc. met. params  ( e.g. T2, d_2d)
+  ,NXTRA_SITE_D2D  =   20       ! Bosco = +5-4 No. Misc. met. params  ( e.g. T2, d_2d)
+!Bosco  ,NXTRA_SITE_D2D  =  9+8       ! No. Misc. met. params  ( e.g. T2, d_2d)
 
 integer, public, parameter, dimension(NADV_SITE) :: &
   SITE_ADV = [(isite, isite=1,NADV_SITE)]  ! Everything
 
 integer, public, parameter, dimension(NSHL_SITE) :: &
-  SITE_SHL = [(isite, isite=1,NSHL_SITE)]  ! All short-lived species
+  SITE_SHL = [ IXSHL_OH ]  ! All short-lived species
+  !SITE_SHL = [(isite, isite=1,NSHL_SITE)]  ! All short-lived species
 
 ! Extra parameters - need to be coded in Sites_mod also. So far
 ! we can choose from hmix, T2, or th (pot. temp.) or d_2d fields.
@@ -64,21 +66,36 @@ integer, public, parameter, dimension(NSHL_SITE) :: &
 character(len=18), public, parameter, dimension(NXTRA_SITE_MISC) :: &
   SITE_XTRA_MISC=[character(len=18):: "th","T2"]
 
+!integer, parameter :: MAX_NEXTRA_SITED2D=100
+!character(len=24), public, save, dimension(MAX_NEXTRA_SITED2D) :: &
+!   site_outputs_extraD2D = '-', sonde_outputs_extraD2D = '-'
+
 !These variables must have been set in My_Derived for them to be used.
 character(len=24), public, parameter, dimension(NXTRA_SITE_D2D) :: &
   SITE_XTRA_D2D=[character(len=24):: &
-    "HMIX","PSURF","ws_10m","rh2m",&
+    "HMIX","PSURF", & ! Bosco skip: "ws_10m","rh2m",&
     "Emis_mgm2_BioNatC5H8","Emis_mgm2_BioNatBIOTERP",&
     "Emis_mgm2_BioNatNO","Emis_mgm2_nox",&
-    'WDEP_PREC',&!'Idirect','Idiffuse','SNratio','SMI_deep',&
-    'met2d_uref','met2d_u10','met2d_rh2m', &
-    'SMI_deep','met2d_SMI_d','SMI_uppr','met2d_SMI_s',&
+    'WDEP_PREC',&!''SNratio',&
+    'met2d_uref','met2d_u10', 'met2d_v10','met2d_rh2m', &
+    !'met2d_SMI1', 'met2d_SMI3',&
+    'met2d_SMI_uppr', 'met2d_SMI_deep',&
+    'met2d_ustar_nwp', 'met2d_LH_Wm2', 'met2d_SH_Wm2',&
+    !BB 'SMI_deep','met2d_SMI_d','SMI_uppr','met2d_SMI_s',&
+!Boscso Extra: +5
+    !BB'USTAR_NWP', 
+    'USTAR_DF','INVL_DF', &
+    'met2d_PARdbh', 'met2d_PARdif' &
+]
+
+!End Boscso Extra:
+  !OLD 'Idirect','Idiffuse' &
 !   "SoilWater_deep","EVAP_CF","EVAP_DF",&
 !   "EVAP_BF","EVAP_NF","WDEP_PREC",&
 !   "RH_GR","CanopyO3_GR","VPD_GR","FstO3_GR",&
 !   "RH_IAM_DF","CanopyO3_IAM_DF","VPD_IAM_DF","FstO3_IAM_DF",&
 !   "COLUMN_CO_k20","COLUMN_C2H6_k20","COLUMN_HCHO_k20","COLUMN_CH4_k20",
-    "COLUMN_NO2_k20"]
+!    "COLUMN_NO2_k20"]
 
 !**** Aircraft outputs   (used in Polinat_mod)
 !==============================================================
@@ -175,6 +192,7 @@ subroutine set_output_defs
 
   real, parameter :: atwC=12.0
   real, parameter :: m_s = 100.0 ! From cm/s to m/s
+  character(len=*), parameter :: dtxt = 'My_Out:set:' ! debug txt
 
   ! introduce some integers to make specification of domain simpler
   ! and less error-prone. Numbers can be changed as desired.
@@ -189,6 +207,10 @@ subroutine set_output_defs
   !   For ugX/m3 output use in combination with to_ug_X(ixadv).
   !==============================================================
   call Init_Units()
+  
+  ! Safety checks for common mistakes:
+  errmsg = find_duplicates(SITE_XTRA_D2D)
+  call CheckStop ( errmsg /= 'no', dtxt//' Duplicate SITE_XTRA_D2D'//errmsg)
 
   !*** Hourly outputs
   !    Note that the hourly output uses **lots** of disc space, so specify
