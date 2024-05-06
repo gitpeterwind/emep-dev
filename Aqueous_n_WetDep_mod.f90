e8be87b18554baffbabd565a57972d2ce3ab71e3
David Simpson <david.simpson@met.no>
2024-05-06 08:00:51 +0000

    small tidy-up in Aqueous_n_WetDep

--- Aqueous_n_WetDep_mod.f90
+++ Aqueous_n_WetDep_mod.f90
@@ -322,8 +322,12 @@ subroutine Setup_Clouds(i,j,debug_flag)
   ksubcloud = KMAX_MID+1       ! k-coordinate of sub-cloud limit
   do k = KMAX_MID, KUPPER, -1
      b(k) = cc3d(i,j,k,1)
-    ! roa(i,j,k,1) * cw_met(i,j,k,1) / b(k) * 1e3 > 0.06 checks that the in-cloud liquid water content is at least that of fog (0.06 g/m3)
-    if(cc3d(i,j,k,1) > B_LIMIT .and. cw_met(i,j,k,1) > CW_LIMIT .and. roa(i,j,k,1) * cw_met(i,j,k,1) / b(k) * 1e3 > 0.06) exit
+    ! roa(i,j,k,1) * cw_met(i,j,k,1) / b(k) * 1e3 > 0.06 checks that the
+    ! in-cloud liquid water content is at least that of fog (0.06 g/m3)
+    if(b(k) > B_LIMIT .and. cw_met(i,j,k,1) > CW_LIMIT ) then
+      if ( roa(i,j,k,1) * cw_met(i,j,k,1) / b(k) * 1e3 > 0.06) exit
+    endif
+    !if(cc3d(i,j,k,1) > B_LIMIT .and. cw_met(i,j,k,1) > CW_LIMIT .and. roa(i,j,k,1) * cw_met(i,j,k,1) / b(k) * 1e3 > 0.06) exit
     ksubcloud = k
   end do
 
@@ -338,12 +342,13 @@ subroutine Setup_Clouds(i,j,debug_flag)
 
 
      do k = KUPPER, ksubcloud-1
-        b(k) = cc3d(i,j,k,1)
+        !b(k) = cc3d(i,j,k,1)
         ! Units: kg(w)/kg(air) * kg(air(m^3) / density of water 10^3 kg/m^3
         ! ==> cloudwater (volume mixing ratio of water to air *in cloud*)
         ! (it is divided by cloud fraction b )
 
-        if(b(k) > B_LIMIT .and. cw_met(i,j,k,1) > CW_LIMIT .and. roa(i,j,k,1) * cw_met(i,j,k,1) / b(k) * 1e3 > 0.06) then
+        if(b(k) > B_LIMIT .and. cw_met(i,j,k,1) > CW_LIMIT) then
+          if( roa(i,j,k,1) * cw_met(i,j,k,1) / b(k) * 1e3 > 0.06) then
            ! value of cloudwater in the cloud fraction of the grid in units
            ! vol(water)/vol(air)
            ! if  FIXED_CLW > 0, this value is used for clouds. Otherwise
@@ -357,6 +362,7 @@ subroutine Setup_Clouds(i,j,debug_flag)
            end if 
            incloud(k) = .true.
            if(kcloudtop<0) kcloudtop = k
+          end if ! > 0.06 kg/kg
         end if
      end do  ! k inside cloud loop
 
